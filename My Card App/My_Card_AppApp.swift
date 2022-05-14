2f151a9fef0929af2c61fd45d701c419580a96eb
Flunx <hookoux@gmail.com>
2022-05-14 21:12:28 +0300

    Initial Commit

---
+++ My Card App/My_Card_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Card_AppApp.swift
+//  My Card App
+//
+//  Created by admin on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct My_Card_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
